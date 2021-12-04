3c1e89d04e18069895274e66818d6fad2b9f2e95
Ulukkek <hofutop@gmail.com>
2021-12-04 17:05:10 +0600

    выполнено

---
+++ script.js
@@ -0,0 +1,21 @@
+/*let camelCase = 0;
+let snake_case = 1;
+let kebabcase = 2;
+let PascalCase = 3;
+alert(camelCase);
+alert(snake_case);
+alert(kebabcase);
+alert(PascalCase);*/
+
+/*let str1 = 'САЛАМ,';
+let str2 = 'БИШКЕК!';
+alert(str1+str2);
+*/
+
+/*let name =' Улук';
+let result = 'Привет,'+name;
+alert(result);*/
+
+/*let age = 17;
+let result = 'Мне '+age+' лет!'
+alert(result);*/
